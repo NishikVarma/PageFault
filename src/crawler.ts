608d0387b63d307d24cd669a0749f89cc0971aad
Nishik <nishikvonline@gmail.com>
2026-03-19 00:53:19 +0530

    feat: autonomous crawling with depth limit, visited set, hostname filtering

--- src/crawler.ts
+++ src/crawler.ts
@@ -17,10 +17,14 @@ import {
 } from "./detectors/index";
 
 const urls = [
-  "https://onlinesbi.sbi.bank.in/",
+  // "https://onlinesbi.sbi.bank.in/",
   "https://www.google.com",
-  "https://www.w3schools.com/html/html_forms.asp",
+  // "https://www.w3schools.com/html/html_forms.asp",
 ];
+
+const stack = urls.map(url => ({url, depth: 0}));
+const visited = new Set<string>();
+const maxdepth = 1;
 
 const detectors = [
   detectVagueLinkText,
@@ -34,17 +38,33 @@ const detectors = [
 ];
 
 (async () => {
+  console.log("Crawling automatically till depth = 1");
   const browser = await firefox.launch();
   const page = await browser.newPage();
 
   const pages: PageReport[] = [];
   var totIssue = 0;
 
-  for (const url of urls) {
+  while (stack.length > 0) {
+    const { url, depth } = stack.pop()!;
+    if(visited.has(url)) continue;
+    visited.add(url);
+
     await page.goto(url);
     const title = await page.title();
     console.log("Checking: " + title);
     const snapshot = await page.locator("body").ariaSnapshot();
+
+    const lines = snapshot.split("\n");
+    for(let i =0 ; i < lines.length -1 ; i++){
+      if(lines[i].includes("- link") && lines[i+1].includes("/url:")){
+        let newUrl = lines[i+1].substring(lines[i+1].indexOf("/url:") + 5).trim();
+        const resolvedUrl = new URL(newUrl, url);
+        if((new URL(url).hostname == resolvedUrl.hostname) && depth < maxdepth){
+          stack.push({url: resolvedUrl.href, depth: depth+1});
+        }
+      }
+    }
 
     const pageIssues: Issue[] = [];
 
